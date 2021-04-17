d66867aff6adf1861782a439ab0c99354120cd1f
Ryan <ryan.atienza97@gmail.com>
2021-04-17 02:49:36 -0700

    beginnings of home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route, Switch, Link } from "react-router-dom";
+import About from './About';
+import Home from './Home';
+import myIcon from './assets/icon.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <nav className="bg-yellow-400 p-8 shadow-xl">
+          <img className="rounded-full min-w-xs border-2 border-white p-2 mb-6" src={myIcon} alt="A drawing of myself that I made"></img>
+            <div className="text-2xl font-medium tracking-wide font-sans pb-6">
+              Ryan Atienza
+            </div>
+            <ul id="navigation">
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+          <Home />
+          </Route>
+          <Route path="/about">
+          <About />
+          </Route>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
