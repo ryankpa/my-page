9c470db58554731064b71ca8bbacda22e589e816
Ryan <ryan.atienza97@gmail.com>
2021-10-27 14:17:16 -0700

    restoring to older version

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import { PrettyHeader } from './PrettyHeader'
-import { About } from './About'
-import { Nav } from './Nav'
 
-class App extends React.Component {
-  render(){
-    return (
-      <div className="App">
-        <Nav />
-        <PrettyHeader />
-        <About />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
