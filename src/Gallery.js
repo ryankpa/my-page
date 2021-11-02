0238e3d7b45c896a19c35848ab2e8c4918931ef9
Ryan <ryan.atienza97@gmail.com>
2021-11-01 20:00:39 -0700

    get project carousel working

--- src/Gallery.js
+++ src/Gallery.js
@@ -4,7 +4,7 @@ function Gallery(props) {
     return (
         <div className="gallery">
             {props.imageArray.map((item, index) => {
-                return <img src={item.image} alt={item.alt_text} />;
+                return <img key={index} src={item.image} alt={item.alt_text} />;
             })
             }
         </div>
